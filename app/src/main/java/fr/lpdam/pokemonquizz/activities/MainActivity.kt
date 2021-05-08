428e8e8905ce2ca44cf6000fc89f1af2b9fbc64a
Marina Cesano <marina.cesano@gmail.com>
2021-05-08 19:48:00 +0200

    Home Page de l'appli

--- app/src/main/java/fr/lpdam/pokemonquizz/activities/MainActivity.kt
+++ app/src/main/java/fr/lpdam/pokemonquizz/activities/MainActivity.kt
@@ -5,12 +5,17 @@ import android.os.Bundle
 import android.util.Log
 import fr.lpdam.pokemonquizz.R
 import fr.lpdam.pokemonquizz.controllers.PokemonController
+import fr.lpdam.pokemonquizz.databinding.ActivityMainBinding
 import fr.lpdam.pokemonquizz.models.Pokemon
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.jeu.setOnClickListener {  }
+        binding.poke.setOnClickListener {  }
     }
 }
